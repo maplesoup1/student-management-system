113c42486e9db1b52362a7395b7798ca55e1897f
maplesoup1 <91361133+maplesoup1@users.noreply.github.com>
2025-07-26 18:16:35 +1000

    edit cors rule

--- backend/src/main/java/com/billieonsite/studentmanagement/config/WebConfig.java
+++ backend/src/main/java/com/billieonsite/studentmanagement/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://student-management-system-gamma-henna.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
